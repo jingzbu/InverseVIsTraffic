be64c1d21578fa36aed54d81c42c994088e1a2f4
jingzbu <jingzbu@gmail.com>
2015-05-25 13:28:43 -0400

    update with v0.44

---
+++ defNewArc.jl
@@ -0,0 +1,12 @@
+type newArc
+    flag::Int
+	initNode::Int
+	termNode::Int 
+    capacity::Float64
+    freeflowtime::Float64
+    trueflow::Float64
+    obsflow::Float64
+end
+
+newArc(flag::Int, initNode::Int, termNode::Int, capacity::Float64,freeflowtime::Float64) = 
+    newArc(flag, initNode, termNode, capacity, freeflowtime, 0., 0.)
