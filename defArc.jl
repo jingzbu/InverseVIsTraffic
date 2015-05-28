28b7ed7d29f8ef3cc93f4bbbf2bbc2b1aa72ccf3
jingzbu <jingzbu@gmail.com>
2015-05-28 13:06:59 -0400

    update with v0.6

--- defArc.jl
+++ defArc.jl
@@ -7,5 +7,5 @@ type Arc
     obsflow::Float64
 end
 
-Arc(initNode::Int, termNode::Int, capacity::Float64,freeflowtime::Float64) = 
+Arc(initNode::Int, termNode::Int, capacity::Float64, freeflowtime::Float64) = 
     Arc(initNode, termNode, capacity, freeflowtime, 0., 0.)
