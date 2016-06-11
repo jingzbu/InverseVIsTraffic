9f019331df7311fc526670671a8ee226488417f5
jingzbu <jingzbu@gmail.com>
2016-06-11 16:58:34 -0400

    debug using a cropped Sioux-Falls network

---
+++ Python_files/create_path-link_incidence_matrix_Sioux_simp.py
@@ -0,0 +1,147 @@
+#!/usr/bin/env python
+
+
+__author__ = "Jing Zhang"
+__email__ = "jingzbu@gmail.com"
+__status__ = "Development"
+
+
+from util import *
+import numpy as np
+from math import exp
+
+import json
+
+
+link_label_dict = zload('../temp_files/link_label_dict_Sioux_simp.pkz')
+link_label_dict_ = zload('../temp_files/link_label_dict_Sioux_simp_.pkz')
+
+with open('../temp_files/link_length_dict_Sioux_simp.json', 'r') as json_file:
+    link_length_dict = json.load(json_file)
+
+# number of links
+m = 20
+# number of routes (obtained by counting the rows with '->' in 'path-link_incidence.txt')
+r = 236
+
+# number of O-D pairs
+s = 9 * (9 - 1)
+
+# initialize the path-link incidence matrix
+A = np.zeros((m, r))
+
+# read in the manually created path-link incidence file 
+# create path-link incidence matrix A
+with open('../temp_files/path-link_incidence_Sioux_simp.txt', 'r') as the_file:
+    # path counts
+    i = 0  
+    for row in the_file:
+        if '->' in row:
+            for j in range(m):
+                if link_label_dict[str(j)] in row:
+                    A[j, i] = 1
+            i = i + 1
+    assert(i == r)
+zdump(A, '../temp_files/path-link_incidence_matrix_Sioux_simp.pkz')
+
+# link_length_dict['0'].length
+
+# link_label_dict_
+
+# read in the manually created path-link incidence file 
+# calculate length of each route
+
+length_of_route_list = []
+with open('../temp_files/path-link_incidence_Sioux_simp.txt', 'r') as the_file:
+    for row in the_file:
+        if '->' in row:
+            link_list = []
+            node_list = []
+            for i in row.split('->'):
+                node_list.append(int(i))
+            for i in range(len(node_list))[:-1]:
+                link_list.append('%d->%d' %(node_list[i], node_list[i+1]))
+            length_of_route = sum([link_length_dict[str(link_label_dict_[link])] \
+                                  for link in link_list])
+            length_of_route_list.append(length_of_route)
+zdump(length_of_route_list, '../temp_files/length_of_route_list_Sioux_simp.pkz')
+
+# length_of_route_list[139]
+
+OD_pair_label_dict = zload('../temp_files/OD_pair_label_dict_Sioux_simp.pkz')
+
+# OD_pair_label_dict['(1, 2)']
+
+# read in the manually created path-link incidence file 
+# create label of each route
+OD_pair_route_label_list = []
+OD_pair_idx_list = []
+route_idx_list = []
+with open('../temp_files/path-link_incidence_Sioux_simp.txt', 'r') as the_file:
+    route_idx = 0
+    for row in the_file:
+        if '->' in row:
+            node_list = []
+            for i in row.split('->'):
+                node_list.append(int(i))
+            OD_pair_idx = OD_pair_label_dict[str((node_list[0], node_list[-1]))]
+            OD_pair_idx_list.append(OD_pair_idx)
+            route_idx_list.append(route_idx)
+            OD_pair_route_label_list.append((OD_pair_idx, route_idx))
+            route_idx += 1
+
+OD_pair_route_dict = {}
+
+for i in range(s):
+    route_list = []
+    for r_ in range(r):
+        if OD_pair_idx_list[r_] == i:
+            route_list.append(r_)
+    OD_pair_route_dict[str(i)] = route_list
+zdump(OD_pair_route_dict, '../temp_files/OD_pair_route_dict_Sioux_simp.pkz')
+
+# OD_pair_route_dict['6']
+
+
+# calculate route choice probability matrix P
+# logit choice parameter
+theta = 0.5
+
+P = np.zeros((s, r))
+for i in range(s):
+    for r in OD_pair_route_dict[str(i)]:
+        P[i, r] = 1
+        # P[i, r] = exp(- theta * length_of_route_list[r]) / \
+        #             sum([exp(- theta * length_of_route_list[j]) \
+        #                  for j in OD_pair_route_dict[str(i)]])
+zdump(P, '../temp_files/OD_pair_route_incidence_Sioux_simp.pkz')
+# zdump(P, '../temp_files/logit_route_choice_probability_matrix_Sioux.pkz')
+
+# print(A[0, :])
+# print(sum(A[0, :]))
+# print(np.size(A, 0))
+# print(np.size(A, 1))
+# print(sum(A[:, 0]))
+# print(sum(A[:, 1]))
+# print(sum(A[:, 100]))
+# print(len(A[:, 3]))
+
+# length_of_route_list[3], length_of_route_list[4], length_of_route_list[5]
+
+# sum(P[2,:])
+
+# link_list = []
+# node_list = []
+# for i in '1->2->3->5->4'.split('->'):
+#     node_list.append(int(i))
+# for i in range(len(node_list))[:-1]:
+#     link_list.append('%d->%d' %(node_list[i], node_list[i+1]))
+
+# node_list
+# link_list
+# range(5)[:-1]
+# link_label_dict
+# A[23]
+# np.size(A, 1)
+# B = np.ones((r, 2))
+# np.dot(A, B)
---
+++ Python_files/print_O-D_pair_link_labels_Sioux_simp.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+
+__author__ = "Jing Zhang"
+__email__ = "jingzbu@gmail.com"
+__status__ = "Development"
+
+from util_data_storage_and_load import *
+
+# number of nodes
+n = 9
+
+# create O-D pair labels
+# create a dictionary mapping O-D pairs to labels
+OD_pair_label_dict = {}
+OD_pair_label_dict_ = {}
+with open('../temp_files/O-D_pair_labels_Sioux_simp.txt', 'w') as the_file:
+    label = 0
+    for i in range(n + 1)[1:]:
+        for j in range(n + 1)[1:]:
+            if i != j: 
+                key = (i, j)
+                OD_pair_label_dict[str(key)] = label
+		OD_pair_label_dict_[str(label)] = key
+                the_file.write("O-D pair (%d, %d) ---> label %d    \n" %(i, j, label))
+                label = label + 1
+                if label % 4 == 0:
+                    the_file.write('\n')
+
+zdump(OD_pair_label_dict, '../temp_files/OD_pair_label_dict_Sioux_simp.pkz')
+zdump(OD_pair_label_dict_, '../temp_files/OD_pair_label_dict__Sioux_simp.pkz')
+
+
+from collections import defaultdict
+
+
+# number of links
+m = 20
+
+# create link labels
+# create a dictionary mapping directed links to labels
+link_label_dict = {}
+link_label_dict_ = {}
+
+link_list = ['1->2','1->3','2->1','2->6','3->1','3->4','4->3','4->5','5->4','5->6',
+	     '5->9','6->2','6->5','6->8','7->8','8->6','8->7','8->9','9->5','9->8']
+
+for i in range(m):
+    link_label_dict[str(i)] = link_list[i]
+
+for i in range(m):
+    link_label_dict_[link_list[i]] = i
+
+zdump(link_label_dict, '../temp_files/link_label_dict_Sioux_simp.pkz')
+zdump(link_label_dict_, '../temp_files/link_label_dict_Sioux_simp_.pkz')
