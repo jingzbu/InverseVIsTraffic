a63b974cd33002eca6f21f94b1158de2077d9e32
jingzbu <jingzbu@gmail.com>
2016-06-22 22:33:02 -0400

    plot inline for inverse VI python version

--- Python_files/util.py
+++ Python_files/util.py
@@ -4,6 +4,7 @@ from numpy.linalg import inv, matrix_rank
 from numpy import linalg as LA
 from gurobipy import *
 import json
+import scipy.misc as sc
 
 try:
     from load_dicts import tmc_length_dict, tmc_ref_speed_dict, tmc_length_dict_ext, tmc_ref_speed_dict_ext
