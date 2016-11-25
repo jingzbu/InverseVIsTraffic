6f9e46c168f128ae4cee4dabaf0321b1bb978ba6
jingzbu <jingzbu@gmail.com>
2016-11-25 15:43:44 -0500

    finish poa calculations for all days of 04/2012; visualize the results

---
+++ 03_O-D_matrix_estimation_journal/Apr_weekend_02_OD_matrix_estimation_GLS.py
@@ -0,0 +1,210 @@
+from util import *
+from util_data_storage_and_load import *
+import numpy as np
+from numpy.linalg import inv
+from scipy.sparse import csr_matrix, csc_matrix
+import json
+
+with open('../temp_files/new_route_dict_journal.json', 'r') as json_file:
+    new_route_dict = json.load(json_file)
+
+number_of_routes = len(new_route_dict)
+
+link_label_dict = zload('../temp_files/link_label_dict_journal.pkz')
+
+number_of_links = len(link_label_dict)
+
+# implement GLS method to estimate OD demand matrix
+def GLS(x, A, L):
+    """
+    x: sample matrix, each column is a link flow vector sample; number_of_links * K
+    A: path-link incidence matrix
+    P: logit route choice probability matrix
+    L: dimension of xi
+    ----------------
+    return: xi
+    ----------------
+    """
+    K = np.size(x, 1)
+    S = samp_cov(x)
+
+    #print("rank of S is: \n")
+    #print(matrix_rank(S))
+    #print("sizes of S are: \n")
+    #print(np.size(S, 0))
+    #print(np.size(S, 1))
+
+    inv_S = inv(S).real
+
+    A_t = A.transpose()
+
+    Q_ = A_t * inv_S * A
+    Q_ = Q_.real
+    #Q = adj_PSD(Q_).real  # Ensure Q to be PSD
+    Q = Q_
+
+    #print("rank of Q is: \n")
+    #print(matrix_rank(Q))
+    #print("sizes of Q are: \n")
+    #print(np.size(Q, 0))
+    #print(np.size(Q, 1))
+
+    b = sum([A_t * inv_S * x[:, k] for k in range(K)])
+    # print(b[0])
+    # assert(1==2)
+
+    model = Model("OD_matrix_estimation")
+
+    xi = []
+    for l in range(L):
+        xi.append(model.addVar(name='xi_' + str(l)))
+
+    model.update()
+
+    # Set objective: (K/2) xi' * Q * xi - b' * xi
+    obj = 0
+    for i in range(L):
+        for j in range(L):
+            obj += (1.0 / 2) * K * xi[i] * Q[i, j] * xi[j]
+    for l in range(L):
+        obj += - b[l] * xi[l]
+    model.setObjective(obj)
+
+    # Add constraint: xi >= 0
+    for l in range(L):
+        model.addConstr(xi[l] >= 0)
+        #model.addConstr(xi[l] <= 5000)
+    #fictitious_OD_list = zload('../temp_files/fictitious_OD_list')
+    #for l in fictitious_OD_list:
+        #model.addConstr(xi[l] == 0)
+    model.update()
+
+    #model.setParam('OutputFlag', False)
+    model.optimize()
+
+    xi_list = []
+    for v in model.getVars():
+        # print('%s %g' % (v.varName, v.x))
+        xi_list.append(v.x)
+    # print('Obj: %g' % obj.getValue())
+    return xi_list
+
+# implement GLS method to estimate OD demand matrix
+def GLS_with_known_P(x, A, P, L):
+    """
+    x: sample matrix, each column is a link flow vector sample; 24 * K
+    A: path-link incidence matrix
+    P: logit route choice probability matrix
+    L: dimension of lam
+    ----------------
+    return: lam
+    ----------------
+    """
+    K = np.size(x, 1)
+    S = samp_cov(x)
+
+    #print("rank of S is: \n")
+    #print(matrix_rank(S))
+    #print("sizes of S are: \n")
+    #print(np.size(S, 0))
+    #print(np.size(S, 1))
+
+    inv_S = inv(S).real
+
+    A_t = A.transpose()
+    P_t = P.transpose()
+
+    # PA'
+    PA_t = P * A_t
+
+    # AP_t
+    AP_t = PA_t.transpose()
+
+    Q_ = PA_t * inv_S * AP_t
+    Q_ = Q_.real
+    #Q = adj_PSD(Q_).real  # Ensure Q to be PSD
+    Q = Q_
+
+    b = sum([PA_t * inv_S * x[:, k] for k in range(K)])
+
+
+    model = Model("OD_matrix_estimation")
+
+    lam = []
+    for l in range(L):
+        lam.append(model.addVar(name='lam_' + str(l)))
+
+    model.update()
+
+    # Set objective: (K/2) lam' * Q * lam - b' * lam
+    obj = 0
+    for i in range(L):
+        for j in range(L):
+            obj += (1.0 / 2) * K * lam[i] * Q[i, j] * lam[j]
+    for l in range(L):
+        obj += - b[l] * lam[l]
+    model.setObjective(obj)
+
+    # Add constraint: lam >= 0
+    for l in range(L):
+        model.addConstr(lam[l] >= 0)
+
+    model.update()
+
+    model.setParam('OutputFlag', False)
+    model.optimize()
+
+    lam_list = []
+    for v in model.getVars():
+        # print('%s %g' % (v.varName, v.x))
+        lam_list.append(v.x)
+    # print('Obj: %g' % obj.getValue())
+    return lam_list
+
+# load link_route incidence matrix
+A = zload('../temp_files/link_route_incidence_matrix_journal.pkz')
+A = A.todense()
+
+# load link counts data
+with open('../temp_files/link_day_minute_Apr_dict_journal_JSON.json', 'r') as json_file:
+    link_day_minute_Apr_dict_JSON = json.load(json_file)
+
+weekend_Apr_list = [1, 7, 8, 14, 15, 21, 22, 28, 29]
+# weekend_Apr_list = [9, 10, 11, 12, 13]
+
+feasible_link_dict = zload('../temp_files/feasible_link_dict_journal.pkz')
+
+link_day_minute_Apr_list = []
+for link_idx in [feasible_link_dict[idx] for idx in range(len(feasible_link_dict))]:
+    for day in weekend_Apr_list:
+        for minute_idx in range(120):
+            key = 'link_' + str(link_idx) + '_' + str(day)
+            link_day_minute_Apr_list.append(link_day_minute_Apr_dict_JSON[key] ['PM_flow_minute'][minute_idx])
+
+x = np.matrix(link_day_minute_Apr_list)
+x = np.matrix.reshape(x, len(feasible_link_dict), 1080)
+# x = np.matrix.reshape(x, len(feasible_link_dict), 600)
+
+# print(np.size(x,0), np.size(x,1))
+
+x = np.nan_to_num(x)
+# print(np.size(x,0), np.size(x,1))
+
+# y = np.array(np.transpose(x))
+# y = y[np.all(y != 0, axis=1)]
+# x = np.transpose(y)
+# x = np.matrix(x)
+
+# print(np.size(x,0), np.size(x,1))
+# print(x[:,:2])
+# print(np.size(A,0), np.size(A,1))
+
+# load logit_route_choice_probability_matrix
+P = zload('../temp_files/OD_pair_route_incidence_journal.pkz')
+P = P.todense()
+
+L = np.size(P, 1)  # dimension of xi
+assert(L == number_of_routes)
+
+# xi_list = GLS(x, A, number_of_routes)
+lam_list = GLS_with_known_P(x, A, P, number_of_routes)
---
+++ 03_O-D_matrix_estimation_journal/Apr_weekend_03_OD_matrix_estimation_GLS_julia.jl
@@ -0,0 +1,116 @@
+using PyCall
+unshift!(PyVector(pyimport("sys")["path"]), "")
+@pyimport Apr_weekend_02_OD_matrix_estimation_GLS
+
+# x = Apr_weekend_02_OD_matrix_estimation_GLS.x; # sample matrix, each column is a link flow vector
+# sample; number_of_links * K
+# S = Apr_weekend_02_OD_matrix_estimation_GLS.S; # sample covariance matrix
+# A = Apr_weekend_02_OD_matrix_estimation_GLS.A; # link_route incidence matrix
+P = Apr_weekend_02_OD_matrix_estimation_GLS.P; # route_choice_probability_matrix
+# Q = Apr_weekend_02_OD_matrix_estimation_GLS.Q;
+L = Apr_weekend_02_OD_matrix_estimation_GLS.L; # dimension of xi
+number_of_routes = Apr_weekend_02_OD_matrix_estimation_GLS.number_of_routes;
+number_of_links = Apr_weekend_02_OD_matrix_estimation_GLS.number_of_links;
+
+# P
+
+# A = sparse(A);
+# P = sparse(P);
+# Q = sparse(Q);
+
+# K = size(x, 2)
+
+# inv_S = inv(S)
+
+# A_t = transpose(A)
+
+# b = sum([A_t * inv_S * x[:, k] for k = 1:K])
+
+using JuMP
+
+# model = Model(solver=IpoptSolver())
+
+# @variable(model, xi[1:L] >= 0)
+
+# # Set objective: (K/2) xi' * Q * xi - b' * xi
+# obj = 0
+# for i = 1:L
+#     for j = 1:L
+#         obj += (1.0 / 2) * K * xi[i] * Q[i, j] * xi[j]
+#     end
+# end
+# for l = 1:L
+#     obj += - b[l] * xi[l]
+# end
+
+# @NLobjective(model, Min, obj)
+
+# solve(model)
+
+# xi_list = getvalue(xi)
+
+# xi_list = Apr_weekend_02_OD_matrix_estimation_GLS.xi_list
+lam_list = Apr_weekend_02_OD_matrix_estimation_GLS.lam_list
+
+println("The demand vector lam is:")
+println(lam_list)
+
+outfile = open("../temp_files/od_demand_vector_simplified_journal_Apr_weekend.json",
+"w")
+
+JSON.print(outfile, lam_list)
+
+close(outfile)
+
+#=
+for idx = 1:L
+    if xi_list[idx] < 1e-1
+        xi_list[idx] = 0
+    end
+end
+
+# for idx = 1:L
+#     println(xi_list[idx])
+# end
+
+mGLSJulia = Model()
+
+@variable(mGLSJulia, lam[1:size(P,1)] >= 0)
+
+@variable(mGLSJulia, p[1:size(P,1), 1:size(P,2)] >= 0)
+
+for i = 1:size(P,1)
+    for j = 1:size(P,2)
+        if P[i,j] == 0
+            @constraint(mGLSJulia, p[i,j] == 0)
+        end
+    end
+end
+
+for i = 1:size(P,1)
+    @NLconstraint(mGLSJulia, sum{p[i,j], j = 1:size(P,2)} == 1)
+end
+
+for l = 1:L
+    @NLconstraint(mGLSJulia, sum{p[i,l] * lam[i], i = 1:size(P,1)}
+    == xi_list[l])
+end
+
+# play no actual role, but could not use zero objective
+@NLobjective(mGLSJulia, Min, sum{p[1,j], j = 1:size(P,2)})
+
+solve(mGLSJulia)
+
+println("The demand vector lam is:")
+println(getvalue(lam))
+
+outfile = open("../temp_files/od_demand_vector_simplified_journal_Apr_weekend.json",
+"w")
+
+JSON.print(outfile, getvalue(lam))
+
+close(outfile)
+
+println("The optimal objective function value of (P2) is:")
+println(getobjectivevalue(mGLSJulia))
+=#
